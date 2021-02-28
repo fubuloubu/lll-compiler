f46a6e5d6d480cfc348973f68f3d5db9a138482d
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2021-02-28 15:32:10 -0500

    refactor: upgrade to node-utils v0.1.1

--- lll_optimizer.py
+++ lll_optimizer.py
@@ -1,16 +1,16 @@
-from typing import Dict, Optional, Set
+from typing import Dict, Set
 
 import inspect
 import sys
 
 from errors import CompilerError
-from node_utils import NodeMutator
+from node_utils import ExplorationError, NodeOptimizer
 
 import lll_ast
 from lll_compiler import VM, AVAILABLE_VMS, DEFAULT_VM
 
 
-optimization_1: NodeMutator = NodeMutator(lll_ast.LLLNode)
+optimization_1: NodeOptimizer = NodeOptimizer(lll_ast.LLLNode)
 
 
 @optimization_1.register(lll_ast.LLLNode)
@@ -19,10 +19,10 @@ def optimize_Node(node: lll_ast.LLLNode, vm: VM) -> lll_ast.LLLNode:
 
 
 # Used to select/de-select optimizations to run
-OPTIMIZATIONS: Dict[str, NodeMutator] = {
+OPTIMIZATIONS: Dict[str, NodeOptimizer] = {
     optimization.lower(): optimization
     for optimization_name, optimization in inspect.getmembers(
-        sys.modules[__name__], predicate=lambda o: isinstance(o, NodeMutator)
+        sys.modules[__name__], predicate=lambda o: isinstance(o, NodeOptimizer)
     )
 }
 
@@ -44,6 +44,14 @@ def optimize(
     for _ in range(num_iterations):
         for name, optimization in OPTIMIZATIONS.items():
             if name not in skip_optimizations:
-                node = optimization.update(node, vm)
+                try:
+                    optimized_node = optimization.update(node, vm)
+                except ExplorationError as e:
+                    raise CompilerError("Optimization Failed") from e
+
+                if optimized_node is None:
+                    raise CompilerError("Optimization Failed")
+                else:
+                    node = optimized_node
 
     return node
